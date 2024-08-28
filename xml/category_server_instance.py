837214e64eea7bf12f339348de680b9025dd3b92
Fadzi Ushewokunze <kingfadzi@gmail.com>
2024-08-28 13:28:43 -0400

    xml treez

--- xml/category_server_instance.py
+++ xml/category_server_instance.py
@@ -6,11 +6,12 @@ tree = ET.parse(file_path)
 
 def count_apps_by_mgmnt_group(tree):
     data = []
-    mgmnt_groups = tree.xpath('//MgmntGroup')
+
+    mgmnt_groups = tree.xpath('//Entry/RESULT/MgmntGroup')
 
     for mgmnt_group in mgmnt_groups:
         group_name = mgmnt_group.get('name')
-        app_count = len(mgmnt_group.xpath('.//Application'))
+        app_count = len(mgmnt_group.xpath('.//Business/Area/Application'))
         data.append({'MgmntGroup': group_name, 'ApplicationCount': app_count})
 
     return data

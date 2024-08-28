ea42240a8d5fc3ed05cc21a72abb8d3c4e13f8e4
Fadzi Ushewokunze <kingfadzi@gmail.com>
2024-08-28 16:39:35 -0400

    xml treez

--- xml/nes_summary_1.py
+++ xml/nes_summary_1.py
@@ -5,33 +5,22 @@ file_path = 'nes_summary.xml'
 output_markdown_file = 'nes_summary.md'  # Output Markdown file
 tree = ET.parse(file_path)
 
-def count_nes_by_region_and_city_with_xpath(tree):
-    results = []
+def count_nes_by_region_and_city(tree):
+    cities = tree.xpath('//Entry//region/city')
 
-    entries = tree.xpath('//Entry')
-
-    for entry in entries:
-        entry_id = entry.get('id')
-
-        region_city_pairs = entry.xpath('.//region/@env | .//city/@inst')
-
-        for region_env in set(region_city_pairs[::2]):
-            cities = entry.xpath(f'.//region[@env="{region_env}"]/city')
-
-            for city in cities:
-                city_inst = city.get('inst')
-                nes_count = len(city.xpath('./nes'))
-
-                results.append({
-                    'Environment': entry_id,
-                    'Region': region_env,
-                    'City': city_inst,
-                    'NES Count': nes_count
-                })
+    results = [
+        {
+            'Environment': city.xpath('ancestor::Entry/@id')[0],
+            'Region': city.xpath('ancestor::region/@env')[0],
+            'City': city.get('inst'),
+            'NES Count': len(city.xpath('./nes'))
+        }
+        for city in cities
+    ]
 
     return results
 
-nes_counts = count_nes_by_region_and_city_with_xpath(tree)
+nes_counts = count_nes_by_region_and_city(tree)
 
 df = pd.DataFrame(nes_counts)
 

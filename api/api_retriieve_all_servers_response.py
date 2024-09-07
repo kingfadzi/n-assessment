d019efdea6363011604e6a3ab8b1beeac8b3740c
Fadzi Ushewokunze <kingfadzi@gmail.com>
2024-09-07 11:55:14 -0400

    process tree

--- api/api_retriieve_all_servers_response.py
+++ api/api_retriieve_all_servers_response.py
@@ -1,14 +1,20 @@
 import json
 import pandas as pd
 
+# Load JSON from a file
 with open('data.json', 'r') as f:
     data = json.load(f)
 
+# Flatten the JSON data
 df = pd.json_normalize(data['list'])
 
-df['serverGroups'] = df['serverGroups'].apply(lambda x: ','.join(x))
+# Explode 'serverGroups' into rows
+df = df.explode('serverGroups')
+
+# Rename the 'serverGroups' column to 'serverGroup'
 df.rename(columns={'serverGroups': 'serverGroup'}, inplace=True)
 
+# Save to CSV
 df.to_csv('flattened_data.csv', index=False)
 
 print(df)

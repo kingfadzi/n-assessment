5b9803bc290e9fa0401e56431aed345d647e07ce
kingfadzi <kingfadzi@gmail.com>
2024-09-19 16:16:24 -0400

    Update get_giant_table.sh

--- get_giant_table.sh
+++ get_giant_table.sh
@@ -8,17 +8,23 @@ DB="your_db"
 
 # Check if the necessary parameters for the table are provided
 if [ $# -lt 2 ]; then
-  echo "Usage: $0 <table> <datecol> [limit]"
+  echo "Usage: $0 <table> <datecol> [period] [limit]"
   exit 1
 fi
 
 # Command line parameters for variable inputs
 TABLE=$1
 DATECOL=$2
-LIMIT=$3  # This is optional
+PERIOD=$3   # Optional
+LIMIT=$4    # Optional
 
 # Build the command
 CMD="python get_giant_table.py --host $HOST --instance $INSTANCE --port $PORT --db $DB --table $TABLE --datecol $DATECOL"
+
+# Include period if provided
+if [[ ! -z "$PERIOD" ]]; then
+  CMD="$CMD --period $PERIOD"
+fi
 
 # Include limit if provided
 if [[ ! -z "$LIMIT" ]]; then
